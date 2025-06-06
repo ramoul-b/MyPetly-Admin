3d42bbc52c2fdb2aac6f2f116ebd624d329cf11b
RamoulB <badr.ramoul@gmail.com>
2025-06-06 21:10:28 +0200

    Add provider CRUD module

--- src/app/store.js
+++ src/app/store.js
@@ -2,13 +2,19 @@ import { configureStore } from '@reduxjs/toolkit'
 import { authApi } from '../modules/auth/authApi'
 import authReducer from '../modules/auth/authSlice'
 import { animalsApi } from '../modules/animals/animalsApi'
+import { providerApi } from '../modules/provider/providerApi'
 
 export default configureStore({
   reducer: {
     [authApi.reducerPath]: authApi.reducer,
     [animalsApi.reducerPath]: animalsApi.reducer,
+    [providerApi.reducerPath]: providerApi.reducer,
     auth: authReducer
   },
   middleware: (getDefault) =>
-    getDefault().concat(authApi.middleware, animalsApi.middleware)
+    getDefault().concat(
+      authApi.middleware,
+      animalsApi.middleware,
+      providerApi.middleware
+    )
 })
