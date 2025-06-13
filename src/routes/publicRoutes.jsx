ab848bf61d6f705e6833043baf1d550ccdc07c1d
RamoulB <badr.ramoul@gmail.com>
2025-06-13 23:34:37 +0200

    Add /403 route

--- src/routes/publicRoutes.jsx
+++ src/routes/publicRoutes.jsx
@@ -1,6 +1,8 @@
 // routes/publicRoutes.js
 import LoginPage from '../modules/auth/LoginPage'
+import ForbiddenPage from '../pages/ForbiddenPage'
 
 export default [
-  { path: '/login', element: <LoginPage /> }
+  { path: '/login', element: <LoginPage /> },
+  { path: '/403', element: <ForbiddenPage /> }
 ]
